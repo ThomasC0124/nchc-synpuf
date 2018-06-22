9db52254239665522cac644cbdacb1f6e979d2d6
KY Chen <b97501055@gmail.com>
2018-06-21 22:21:32 -0500

    Updated test functions according to parser behavior changes

--- tests/preprocessing/test_beneficiary_parser.py
+++ tests/preprocessing/test_beneficiary_parser.py
@@ -19,13 +19,13 @@ def test_instantiate_beneficiary_summary_parser(beneficiary_summary_parser, fake
 def test_beneficiary_summary_parser_data_io_on_sample(beneficiary_summary_parser,
                                                       sample_beneficiary_summary_data_fn):
     beneficiary_summary_parser.add_data_file(sample_beneficiary_summary_data_fn)
-    fn_out = './tests/sample_bene_summary.json'
-    beneficiary_summary_parser.parse_data(fn_out)
-    assert os.path.isfile(fn_out)
-    os.remove(fn_out)
+    fn_outs = beneficiary_summary_parser.parse_data()
+    assert len(fn_outs) == 1
+    assert os.path.isfile(fn_outs[0])
+    os.remove(fn_outs[0])
 
 def test_beneficiary_summary_parser_data_io_on_fake(beneficiary_summary_parser,
                                                     fake_beneficiary_summary_data_fn):
     beneficiary_summary_parser.add_data_file(fake_beneficiary_summary_data_fn)
-    fn_out = './tests/sample_bene_summary.json'
-    beneficiary_summary_parser.parse_data(fn_out)
+    # TODO: make sure logging messages are captured
+    fn_outs = beneficiary_summary_parser.parse_data()
